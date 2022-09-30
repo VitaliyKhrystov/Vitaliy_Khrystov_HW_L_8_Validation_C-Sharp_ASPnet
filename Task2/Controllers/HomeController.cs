49ae7cc502de19d263f68b921c5b54f058011a7f
Vitalii Khrystov <vitalik_khristov@ukr.net>
2022-09-30 15:31:26 +0300

    Created Task 2

---
+++ Task2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Task2.Models;
+
+namespace Task2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string data)
+        {
+            return View((object)data);
+        }
+
+      
+    }
+}
---
+++ Task2/Models/PersonRegister.cs
@@ -0,0 +1,27 @@
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Task2.Models
+{
+    public class PersonRegister
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        [Display(Name = "Ім'я")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        [Display(Name = "Фамілія")]
+        public string Surname { get; set; }
+
+        [Required]
+        [Email]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
