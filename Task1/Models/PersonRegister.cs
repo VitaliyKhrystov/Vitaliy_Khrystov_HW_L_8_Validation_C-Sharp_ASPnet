fed98ecd863718b878cca84d555ec986f7c3d43a
Vitalii Khrystov <vitalik_khristov@ukr.net>
2022-09-30 13:48:47 +0300

    Add project files.

---
+++ Task1/Models/PersonRegister.cs
@@ -0,0 +1,25 @@
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Task1.Models
+{
+    public class PersonRegister
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string Surname { get; set; }
+
+        [Required]
+        [Email]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,47 @@
+namespace Task1
+{
+    public class Program
+    {
+
+        //Задание 1 (Additional Task)
+        //Создайте форму регистрации на консультацию.В форме должны быть поля:
+        //Имя Фамилия
+        //Email
+        //Желаемая дата консультации
+        //Все поля обязательные для ввода.Email должен быть в соответствующем формате. Дата
+        //консультации должна быть правильной датой, она должна быть в будущем и не должна
+        //попадать на выходные дни.
+        //При наличии ошибок, необходимо оповестить пользователя об ошибках.
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
